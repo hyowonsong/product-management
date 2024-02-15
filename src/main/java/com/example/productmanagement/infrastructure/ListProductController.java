e5540a7b1b557341213afa44c0e3aac2f24e5645
hyowonsong <woniwoni30@gmail.com>
2024-02-15 21:05:15 +0900

    [Add] 인프라스트럭처 계층 추가

--- src/main/java/com/example/productmanagement/infrastructure/ListProductController.java
+++ src/main/java/com/example/productmanagement/infrastructure/ListProductController.java
@@ -1,2 +1,18 @@
-package com.example.productmanagement.infrastructure;public class ListProductController {
+package com.example.productmanagement.infrastructure;
+
+import com.example.productmanagement.domain.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+@Repository
+public class ListProductController {
+
+    private List<Product>  products = new CopyOnWriteArrayList<>();
+
+    public Product add(Product product){
+        products.add(product);
+        return product;
+    }
 }
