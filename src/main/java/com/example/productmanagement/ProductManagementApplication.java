b123bd96f4e319eb672126659749f6f3f835456a
hyowonsong <woniwoni30@gmail.com>
2024-02-16 16:35:46 +0900

    [Update] ModelMapper 추가

--- src/main/java/com/example/productmanagement/ProductManagementApplication.java
+++ src/main/java/com/example/productmanagement/ProductManagementApplication.java
@@ -1,7 +1,9 @@
 package com.example.productmanagement;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ProductManagementApplication {
@@ -10,4 +12,8 @@ public class ProductManagementApplication {
 		SpringApplication.run(ProductManagementApplication.class, args);
 	}
 
+	@Bean
+	public ModelMapper modelMapper({
+		return new ModelMapper();
+	}
 }
