11cfb2133b1bd90dab17271b103aaa05047d600d
hyowonsong <woniwoni30@gmail.com>
2024-02-16 16:33:08 +0900

    [Add] Dto 추가

--- src/main/java/com/example/productmanagement/presentation/ProductDto.java
+++ src/main/java/com/example/productmanagement/presentation/ProductDto.java
@@ -1,2 +1,24 @@
-package com.example.productmanagement.presentation;public class ProductDto {
+package com.example.productmanagement.presentation;
+
+public class ProductDto {
+    private Long id;
+    private String name;
+    private Integer price;
+    private Integer amount;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
 }
