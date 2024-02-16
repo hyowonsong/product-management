4fa93fb0014a147fd81f3ff15ce670f0f46d68db
hyowonsong <woniwoni30@gmail.com>
2024-02-16 18:01:54 +0900

    [Update] 컨트롤러 유효성 검사

--- src/main/java/com/example/productmanagement/presentation/ProductController.java
+++ src/main/java/com/example/productmanagement/presentation/ProductController.java
@@ -2,6 +2,7 @@ package com.example.productmanagement.presentation;
 
 import com.example.productmanagement.application.SimpleProductService;
 import com.example.productmanagement.domain.Product;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,7 +19,7 @@ public class ProductController {
     }
 
     @RequestMapping(value = "/products", method = RequestMethod.POST)
-    public ProductDto createProduct(@RequestBody ProductDto productDto){
+    public ProductDto createProduct(@Valid @RequestBody ProductDto productDto){
         return simpleProductService.add(productDto);
     }
 
