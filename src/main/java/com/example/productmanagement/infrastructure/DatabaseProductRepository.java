8abd0bf3ed716459a21889012a27da9fac1a4494
hyowonsong <woniwoni30@gmail.com>
2024-02-16 18:42:58 +0900

    [Add] DatabaseProductRepository 추가

---
+++ src/main/java/com/example/productmanagement/infrastructure/DatabaseProductRepository.java
@@ -0,0 +1,22 @@
+package com.example.productmanagement.infrastructure;
+
+import com.example.productmanagement.domain.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DatabaseProductRepository {
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public DatabaseProductRepository(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+
+    public Product add(Product product){
+        return null;
+    }
+
+}
