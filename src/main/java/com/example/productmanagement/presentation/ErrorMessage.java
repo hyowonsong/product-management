e19761a1247dcbc24d94301d9b72d02f6bc5bd76
hyowonsong <woniwoni30@gmail.com>
2024-02-16 18:09:25 +0900

    [Add] 전역 예외 핸들러 추가하기

---
+++ src/main/java/com/example/productmanagement/presentation/ErrorMessage.java
@@ -0,0 +1,17 @@
+package com.example.productmanagement.presentation;
+
+import java.util.List;
+
+public class ErrorMessage {
+
+    private List<String> errors;
+
+    public ErrorMessage(List<String> errors) {
+        this.errors = errors;
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+
+}
